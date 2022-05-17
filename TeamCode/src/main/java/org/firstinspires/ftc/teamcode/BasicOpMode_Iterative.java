52f9c0f2e7472f6e50144e7f31cb062ea7e4a510
ad25343 <39708162+ad25343@users.noreply.github.com>
2022-05-16 20:51:33 -0400

    Test GIT by adding a new teleop program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -50,6 +50,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+/*Test*/
+
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 @Disabled
 public class BasicOpMode_Iterative extends OpMode
